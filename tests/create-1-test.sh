3552d47c01ee86af79fea97859c1532093a046da
Lee Thompson <stagr.lee@gmail.com>
2013-02-01 04:36:29 -0600

    add create command

---
+++ tests/create-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `create` command.
+#    
+#/ usage:  rerun stubbs:test -m aws -p create [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "create"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
